72583c9bee0b3f4a9903e65fe92a3816f847f606
LuisPLima <lplyma@gmail.com>
2025-06-11 16:41:49 -0300

    Delete render-ready/backend directory

--- render-ready/backend/leitura_qrcode.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-from pyzbar.pyzbar import decode
-
-def ler_qrcode(imagem_bytes):
-    nparr = np.frombuffer(imagem_bytes, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    decodificados = decode(img)
-    return decodificados[0].data.decode('utf-8') if decodificados else None
